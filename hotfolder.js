b003f5a340c8c3e2f30e0d3ba939d0fec6f8bb44
Jakob Mann Jensen <jakob@mannen.dk>
2018-12-12 20:42:42 +0100

    hotfolder v1.02

--- hotfolder.js
+++ hotfolder.js
@@ -1,6 +1,28 @@
 var fs = require('fs');
+var glob = require('glob');
 
 module.exports.startHotfolder = function(){
   console.log('Hotfolder running');
-
+  scanHotfolder();
 };
+
+function scanHotfolder(){
+  var getDirectories = function (src, callback) {
+    glob(src + '/**/*', callback);
+  };
+  getDirectories('/Users/jakobmannjensen/Documents/NodeJS', function (err, res) {
+    if (err) {
+      console.log('Error', err);
+    } else {
+      console.log(res);
+    }
+    console.log('length: '+res.length);
+    for(var f in res)
+    {
+      if(res[f].includes('.json'))
+      {
+        console.log('TestNew '+res[f]);
+      }
+    }
+  });
+}
